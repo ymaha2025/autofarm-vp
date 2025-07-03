7e047c2ea73d29fc6fe48fab859955737d8c1b88
ymaha2025 <ymaha2025@outlook.sa>
2025-07-03 12:21:11 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import time
+
+def start_autofarm():
+    while True:
+        print("[✓] AutoFarm يعمل الآن بشكل تلقائي...")
+        time.sleep(60)
+
+if __name__ == "__main__":
+    start_autofarm()
